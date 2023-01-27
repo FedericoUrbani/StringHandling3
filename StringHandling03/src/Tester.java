34bc670b45b9422aaaf656c6bbb18b9d4a3e8bd3
Federico Urbani <urbanifederico5@gmail.com>
2023-01-27 19:58:27 +0100

    updated27

---
+++ StringHandling03/src/Tester.java
@@ -0,0 +1,23 @@
+import java.sql.Array;
+
+public class Tester {
+
+    public static String [] removeFirstAndLastSpace(String [] array){
+            String[] provisionalArray=new String[array.length];
+        for (int i=0;i<array.length;i++){
+
+
+            String result = array[i].trim();
+            provisionalArray[i]=result;
+        }
+       return provisionalArray;
+    }
+    public static void main(String[] args) {
+
+        String[] array1 = {" i want ", " to learn ", " how to code! "};
+        String[] array2 = Tester.removeFirstAndLastSpace(array1);
+        System.out.println(!(array2[0].compareTo("i want")>(array2[1].compareTo("to learn"))));
+
+
+    }
+}
